594ba068b7576554192ae413ad18914e0d9f004f
Sinaaaa <Gabbbbbbbbbbbb@gmail.com>
2026-02-03 09:13:33 +0100

    remove misbehaving super key

--- layout.sina.h
+++ layout.sina.h
@@ -236,9 +236,9 @@ static struct key keys_full[] = {
 
   //{"⌨͕", "⌨͔", 1.5, NextLayer, .scheme = 1},
   {"Ctr", "Ctr", 1.0, Mod, Ctrl, .scheme = 1},
-  {"Sup", "Sup", 1.0, Mod, Super, .scheme = 1},
+  //{"Sup", "Sup", 1.0, Mod, Super, .scheme = 1},
   {"Alt", "Alt", 1.0, Mod, Alt, .scheme = 1},
-  {"", "", 5.0, Code, KEY_SPACE},
+  {"⎵", "⎵", 6.0, Code, KEY_SPACE},
   {"AGr", "AGr", 1.0, Mod, AltGr, .scheme = 1},
   {"Ctr", "Ctr", 1.0, Mod, Ctrl, .scheme = 1},
   {"←", "←", 1.0, Code, KEY_LEFT, .scheme = 1},
